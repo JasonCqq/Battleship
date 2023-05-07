49fcd3f87fc347002a86bccf8d56d11791dfddf2
Jason <121495300+jason21715@users.noreply.github.com>
2023-05-07 01:54:13 -0700

    Add Ship and Gameboard class

--- src/script.js
+++ src/script.js
@@ -1,5 +1,46 @@
-function sum(a, b) {
-  return a + b;
+class Ship {
+  constructor(length, hits = 0, sunk = false) {
+    this.length = length;
+    this.hits = hits;
+    this.sunk = sunk;
+  }
+
+  hit() {
+    this.hits++;
+    this.isSunk();
+  }
+
+  isSunk() {
+    this.hits === this.length ? (this.sunk = true) : false;
+  }
 }
 
-module.exports = sum;
+// Gameboards should be able to place ships at specific coordinates by calling the ship factory function.
+class Gameboard {
+  constructor(missedAttacks = [], allSunk = false) {
+    this.missedAttacks = missedAttacks;
+    this.allSunk = allSunk;
+    this.board;
+  }
+
+  createBoard() {
+    this.board = [
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ];
+  }
+
+  receiveAttack(coordinates) {
+    //check if hit or not, then send the hit function to ship or record coordinates if not hit.
+  }
+}
+
+class Player {}
--- src/script.test.js
+++ src/script.test.js
@@ -1,3 +1,3 @@
-const sum = require("./script");
+// const sum = require("./script");
 
-test("sum 2 numbers together", () => expect(sum(1, 2)).toBe(3));
+// test("sum 2 numbers together", () => expect(sum(1, 2)).toBe(3));
